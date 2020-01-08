6d6276f02e891d34cf6466d4d5c669b8ad963b3c
ashea27 <shea.andrew27@gmail.com>
2020-01-08 10:57:03 -0700

    first commit after deleting/re-initializing repo

---
+++ src/Components/Map/Map.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { GoogleMap, withScriptjs, withGoogleMap } from 'react-google-maps'
+
+
+
+function Map(){
+  return(
+    <GoogleMap 
+      id='map' 
+      defaultZoom={10} 
+      defaultCenter={{lat: 39.739235, lng: -104.990250}}
+    />
+
+  )
+}
+
+const MapComponent = withScriptjs(withGoogleMap(Map))
+
+
+export default function ProspectMap() {
+  // console.log('this is key from inside mapJS', process.env)
+  return (
+    <MapComponent
+      googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_MAPS_KEY}`}
+      loadingElement={<div style={{height: "100%"}}/>}
+      containerElement={<div style={{height: "100%"}}/>}
+      mapElement={<div style={{height: "100%"}}/>}
+    />
+  )
+}
---
+++ src/Components/ProspectDetails/ProspectDetails.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import ProspectMap from '../Map/Map'
+
+
+
+function ProspectDetails() {
+  return(
+    <>
+      <div style={{height: '400px', width: '600px'}}>
+        <ProspectMap />
+        
+      </div>
+    </>
+  )
+}
+
+
+
+export default ProspectDetails
